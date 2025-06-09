a3b1ee9d265407e07c9d48468d6af6b28438c4e0
João Gabriel S. F <jgabsfernandes@gmail.com>
2025-06-09 00:54:13 -0300

    chore: icons and vite path configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
+import path from "path";
 import solidPlugin from 'vite-plugin-solid';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [solidPlugin(), tailwindcss()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src")
+    }
+  },
   server: {
     port: 3000,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:5000',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
   },
   build: {
     target: 'esnext',
