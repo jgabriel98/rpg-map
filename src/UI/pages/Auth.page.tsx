2432e32a5cb624ecbb0e0fc4c2b973609bceea2f
João Gabriel S. F <jgabsfernandes@gmail.com>
2025-06-13 11:54:50 -0300

    feat: change auth method to OTP code

--- src/UI/pages/Auth.page.tsx
+++ src/UI/pages/Auth.page.tsx
@@ -1,7 +1,12 @@
-import { useLocation, useNavigate, useSearchParams } from '@solidjs/router'
-import { createRenderEffect, createSignal } from 'solid-js'
-import { useSession } from '~/contexts/Session.context'
-import { supabase } from '~/lib/supabase'
+import { useLocation, useNavigate, useSearchParams } from '@solidjs/router';
+import { batch, createEffect, createRenderEffect, createSignal, Setter, Show } from 'solid-js';
+import { useSession } from '~/contexts/Session.context';
+import { Button } from '~/lib/solidui/button';
+import { OTPField, OTPFieldGroup, OTPFieldInput, OTPFieldSeparator, OTPFieldSlot } from '~/lib/solidui/otp-field';
+import { supabase } from '~/lib/supabase';
+import { LoadingSpinner } from '../components/loading/LoadingSpinner.component';
+import { TextField, TextFieldErrorMessage, TextFieldInput, TextFieldLabel } from '~/lib/solidui/text-field';
+import { Tooltip, TooltipContent, TooltipTrigger } from '~/lib/solidui/tooltip';
 
 
 const handleAuthToken = async (token_hash: string) => {
@@ -9,22 +14,62 @@ const handleAuthToken = async (token_hash: string) => {
     token_hash: token_hash,
     type: 'magiclink',
   })
-
   if (error) alert(error.message);
-  throw error;
 }
 
 type SearchParams = {
   token_hash: string
-  type: string
+  type?: string
+}
+
+const MIN_RESEND_TIME_SECONDS = 30;
+
+function useCounter<T>(start: T, stepper: ((current: T) => T), delayMs: number) {
+  const [count, setCount] = createSignal<T>();
+  let timeoutRef: ReturnType<typeof setInterval> | undefined;
+
+  const clear = () => {
+    if (timeoutRef) clearTimeout(timeoutRef);
+    setCount(undefined);
+  }
+  const starter = () => {
+    setCount(start as Setter<T>);
+    timeoutRef = setInterval(() => setCount(stepper as Setter<T>), delayMs);
+  }
+
+  return [
+    count,
+    (start: boolean) => start ? starter() : clear()
+  ] as const;
 }
 
 export default function Auth() {
   const [loading, setLoading] = createSignal(false)
-  const [email, setEmail] = createSignal('')
+  const [email, setEmail] = createSignal('');
+  const [emailError, setEmailError] = createSignal(false);
+  const [confirmCode, setConfirmationCode] = createSignal<string>();
+  const [hasSentCode, setHasSentCode] = createSignal(false);
+  const [canResendCode, setCanResendCode] = createSignal(false);
+
+
+  const isSubmitEnabled = () => email() && !emailError() && !loading() && (!hasSentCode() || canResendCode());
+
+  const [resendTimeout, setResendTimeoutActive] = useCounter(MIN_RESEND_TIME_SECONDS, (prev) => prev - 1, 1000);
+  const startResendCountdown = () => batch(() => {
+    setResendTimeoutActive(true)
+    setCanResendCode(false);
+  })
+
+  createEffect(() => {
+    if (resendTimeout() === 0) batch(() => {
+      setResendTimeoutActive(false);
+      setCanResendCode(true);
+    })
+  })
+
+  const submitButonLabel = () => resendTimeout() ? `wait ${resendTimeout()!} seconds to send code again` : "Send confirmation code"
 
   const [searchParams] = useSearchParams<SearchParams>();
-  const location = useLocation();
   const navigate = useNavigate();
   const session = useSession();
 
@@ -32,49 +77,78 @@ export default function Auth() {
     if (session()) navigate('/', { replace: true })
   })
 
-  if (location.pathname == '/auth/confirm' && searchParams.token_hash) {
+  if (searchParams.type == "magiclink" && searchParams.token_hash) {
     handleAuthToken(searchParams.token_hash)
   }
 
-  const handleLogin = async (e: SubmitEvent) => {
-    e.preventDefault();
+  const verifyEmailInput = (value: string) => {
+    const isValid = /^.+@.+\..+$/.test(value) && value.length < 256
+    setEmailError(!isValid);
+    setEmail(value);
+  }
 
+  const handleSendCode = async (e: SubmitEvent) => {
+    e.preventDefault();
     setLoading(true);
+
     const { error } = await supabase.auth.signInWithOtp({
       email: email(),
-      options: { emailRedirectTo: `${window.location.origin}/auth/confirm` }
+      options: { emailRedirectTo: `${window.location.origin}${window.location.pathname}`, },
     })
-
     if (error) alert(error.message);
-    else alert('Check your email for the login link!');
+    else {
+      setHasSentCode(true);
+      startResendCountdown();
+    }
     setLoading(false);
   }
 
+  const handleConfirmCode = async (token: string) => {
+    setConfirmationCode(token);
+    if (token.length < 6) return;
+    const { error } = await supabase.auth.verifyOtp({ email: email(), token, type: 'email' })
+    if (error) {
+      alert(error.message);
+    }
+  }
 
   return (
-    <div class="row flex-center flex">
-      <div class="col-6 form-widget" aria-live="polite">
-        <h1 class="header">RPG Map do gaba</h1>
-        <p class="description">Sign in via magic link with your email below</p>
-        <form class="form-widget" onSubmit={handleLogin}>
-          <div>
-            <label for="email">Email</label>
-            <input
-              id="email"
-              class="inputField"
-              type="email"
-              placeholder="Your email"
-              value={email()}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-            />
-          </div>
-          <div>
-            <button type="submit" class="button block" aria-live="polite">
-              {loading() ? <span>Loading</span> : <span>Send magic link</span>}
-            </button>
-          </div>
-        </form>
-      </div>
+    <div class="flex flex-col max-w-md w-full justify-self-center">
+
+      <p class="description">Sign in via Confirmation Code</p>
+      <form class="form-widget" onSubmit={handleSendCode}>
+        <div>
+          <TextField value={email()} onChange={verifyEmailInput} validationState={emailError() ? "invalid" : "valid"} disabled={hasSentCode()}>
+            <TextFieldLabel for="email">Email</TextFieldLabel>
+
+            <Tooltip>
+              <TooltipTrigger as={() => <TextFieldInput type='email' id="email" placeholder="Email" />} required />
+              <TooltipContent>
+                <TextFieldErrorMessage>Please enter a valid email.</TextFieldErrorMessage>
+              </TooltipContent>
+            </Tooltip>
+          </TextField>
+        </div>
+
+        <Button type="submit" class="button w-full p-0" aria-live="polite" disabled={!isSubmitEnabled()}>
+          {loading() ? <LoadingSpinner class='!size-6' /> : submitButonLabel()}
+        </Button>
+      </form>
+
+      <OTPField maxLength={6} id="otp" onValueChange={handleConfirmCode} class='self-center' value={confirmCode()}>
+        <OTPFieldInput disabled={!hasSentCode()} />
+        <OTPFieldGroup>
+          <OTPFieldSlot index={0} />
+          <OTPFieldSlot index={1} />
+          <OTPFieldSlot index={2} />
+        </OTPFieldGroup>
+        <OTPFieldSeparator />
+        <OTPFieldGroup>
+          <OTPFieldSlot index={3} />
+          <OTPFieldSlot index={4} />
+          <OTPFieldSlot index={5} />
+        </OTPFieldGroup>
+      </OTPField>
     </div>
   )
 }
